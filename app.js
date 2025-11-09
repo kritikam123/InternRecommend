17fea5d59cac58e02de60417cb651c4e68763deb
krishtina-boop <krishtinamaharjan8@gmail.com>
2025-11-09 11:04:30 +0545

    index page with owl carousel completed

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.json({ limit: "10mb" }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/public", express.static("public", { type: "text/css" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
