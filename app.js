8d2717f52f84cb8237d9724da52abbcade5ab162
krishtina-boop <krishtinamaharjan8@gmail.com>
2025-11-12 22:19:38 +0545

    database model created and connection established with db

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const db = require("./model/model.js")
 const app = express();
 
 app.use(express.static("public"));
---
+++ model/model.js
@@ -0,0 +1,315 @@
+const { Sequelize, DataTypes, UUIDV4 } = require("sequelize");
+const db = require("../connection.js");
+const { type } = require("os");
+
+// **MODELS
+//USER MODEL
+const User = db.define("User", {
+  user_id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false,
+  },
+  full_name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  skills: { type: DataTypes.JSON },
+  phone: {
+    type: DataTypes.STRING,
+  },
+  dateOfBirth: { type: DataTypes.STRING },
+  photo: {
+    type: DataTypes.BLOB,
+    defaultValue: "/images/default.jpg",
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "jobseeker",
+  },
+  suspend: { type: DataTypes.BOOLEAN, defaultValue: false },
+  code: { type: DataTypes.STRING },
+  otherDetails: { type: DataTypes.JSON },
+  experience: { type: DataTypes.JSON },
+  education: { type: DataTypes.JSON },
+  language: { type: DataTypes.JSON },
+  awards: { type: DataTypes.JSON },
+  certifications: { type: DataTypes.JSON },
+  resume: { type: DataTypes.BLOB },
+  social_media: { type: DataTypes.JSON },
+});
+
+//Organnization
+const Organization = db.define("Organization", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  name: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  phone: { type: DataTypes.STRING },
+  location: {
+    type: DataTypes.STRING,
+  },
+  logo: {
+    type: DataTypes.BLOB,
+    defaultValue: "/images/default.jpg",
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "organization",
+  },
+  verified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  rejected: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  suspended: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  deleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  code: {
+    type: DataTypes.STRING,
+  },
+  size: {
+    type: DataTypes.STRING,
+  },
+  category: {
+    type: DataTypes.STRING,
+  },
+  website: {
+    type: DataTypes.STRING,
+  },
+  facebookLink: {
+    type: DataTypes.STRING,
+  },
+  linkedin: {
+    type: DataTypes.STRING,
+  },
+  contactPerson: {
+    type: DataTypes.STRING,
+  },
+  contactPersonNum: {
+    type: DataTypes.STRING,
+  },
+  info: {
+    type: DataTypes.TEXT,
+  },
+  intro: {
+    type: DataTypes.TEXT,
+  },
+});
+
+const Admin = db.define("admin", {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: false,
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+  phone: {
+    type: DataTypes.STRING,
+  },
+  location: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "admin",
+  },
+  logo: {
+    type: DataTypes.BLOB,
+    defaultValue: "/images/default.jpg",
+  },
+  suspended: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  facebook: {
+    type: DataTypes.TEXT,
+  },
+  linkedin: {
+    type: DataTypes.TEXT,
+  },
+});
+
+const Job = db.define("job", {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  companyName: {
+    type: DataTypes.STRING,
+  },
+  title: {
+    type: DataTypes.STRING,
+  },
+  noOfOpening: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+  category: {
+    type: DataTypes.STRING,
+  },
+  education: {
+    type: DataTypes.STRING,
+  },
+  experience: {
+    type: DataTypes.STRING,
+  },
+  location: {
+    type: DataTypes.STRING,
+  },
+  expiryDate: {
+    type: DataTypes.STRING,
+  },
+  salary: {
+    type: DataTypes.STRING,
+  },
+  requiredskills: {
+    type: DataTypes.JSON,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  requirements: {
+    type: DataTypes.TEXT,
+  },
+  instruction: {
+    type: DataTypes.TEXT,
+  },
+  banner: {
+    type: DataTypes.STRING,
+    defaultValue: "/images/default.jpg",
+  },
+  featured: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  jobType: {
+    type: DataTypes.STRING,
+  },
+  remoteOnsite: {
+    type: DataTypes.STRING,
+  },
+  suspended: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  deleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+const AppliedJobs = db.define("appliedJobs", {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: false,
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  applicationDate: {
+    type: DataTypes.STRING,
+  },
+  status: {
+    type: DataTypes.ENUM("Pending", "Accepted", "Rejected"),
+    defaultValue: "Pending",
+  },
+  userId: {
+    type: DataTypes.UUID,
+  },
+  cvUrl: {
+    type: DataTypes.STRING,
+  },
+  coverLetter: {
+    type: DataTypes.STRING,
+  },
+});
+
+const SavedJobs = db.define("savedJobs", {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: false,
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+  },
+});
+
+const Contact = db.define("contact", {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: false,
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  fullName: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+  },
+  subject: {
+    type: DataTypes.STRING,
+  },
+  message: {
+    type: DataTypes.TEXT,
+  },
+});
+
+//user and applied jobs relation
+User.hasMany(AppliedJobs);
+AppliedJobs.belongsTo(User);
+
+// job and applied-job
+Job.hasMany(AppliedJobs);
+AppliedJobs.belongsTo(Job);
+
+//Organization and jobs relation
+Organization.hasMany(Job);
+Job.belongsTo(Organization);
+
+Organization.hasMany(AppliedJobs);
+AppliedJobs.belongsTo(Organization);
+
+User.hasMany(SavedJobs);
+SavedJobs.belongsTo(User);
+
+module.exports = {
+  User,
+  Organization,
+  Job,
+  AppliedJobs,
+  SavedJobs,
+  Contact,
+  Admin,
+};
+
+// db.sync({ alter: true });
