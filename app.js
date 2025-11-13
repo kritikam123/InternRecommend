0c8b97b9f6f1c5e355c392cb94b6c225fa737e0a
kritikam123 <kritikam619@gmail.com>
2025-11-13 08:52:51 +0545

    added internship page

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/internship",(req,res)=>{
+  res.render("internship");
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
