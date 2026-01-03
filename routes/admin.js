f2366c6bfc106cad4b55a5519505f270700848de
krishtina-boop <krishtinamaharjan8@gmail.com>
2026-01-03 16:03:45 +0545

    admin edit job completed

--- routes/admin.js
+++ routes/admin.js
@@ -110,15 +110,71 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-router.get("/job-list", async (req, res) => {
+router.get("/job-list", checkuser, async (req, res) => {
   console.log("JOB LIST FOR ADMINNNN");
   const jobs = await Job.findAll({ include: [{ model: Organization }] });
   console.log(jobs);
   res.render("admin/admin-view-job", { jobs: jobs });
 });
 
-router.get("/job-list/edit-job", async (req, res) => {
+router.get("/job-list/edit-job", checkuser, async (req, res) => {
   const jobDetails = await Job.findOne({ where: { id: req.query.id } });
   res.render("admin/edit-job", { jobdetails: jobDetails });
 });
+
+router.post("/job-list/edit-job", checkuser, async (req, res) => {
+  const title = req.body.title;
+  const openeings = req.body.openeings;
+  const category = req.body.category;
+  const jobtype = req.body.jobtype;
+  const remote = req.body.remote;
+  const experience = req.body.experience;
+  const education = req.body.education;
+  const location = req.body.location;
+  const expirey = req.body.expirey;
+  const salary = req.body.salary;
+  const description = req.body.description;
+  const requirements = req.body.requirements;
+  const email = req.user.email;
+  const skills = req.body["skills"];
+  const parsedSkills = Array.isArray(skills) ? skills : skills ? [skills] : [];
+
+  console.log("====================================");
+  console.log("QUERYYYYYYYYYYYYYYYYYYYYYYYY", req.query.id);
+  console.log("====================================");
+  try {
+    const job = await Job.update(
+      {
+        title: title,
+        openeings: openeings,
+        category: category,
+        jobType: jobtype,
+        remote: remote,
+        experience: experience,
+        education: education,
+        expirey: expirey,
+        salary: salary,
+        description: description,
+        requirements: requirements,
+        requiredskills: parsedSkills,
+        OrganizationId: req.user.id,
+      },
+      {
+        where: { id: req.query.id },
+      }
+    );
+    return res.json({
+      status: 200,
+      title: "success",
+      message: "Job Updated Successfully",
+    });
+  } catch (err) {
+    console.log("error while updating job", err);
+    return res.json({
+      title: "failed",
+      message: "something went wrong while updating",
+    });
+  }
+});
+
 module.exports = router;
